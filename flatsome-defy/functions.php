bd3ecd93a0d5967c760c58451c4e0183dec022b2
linda@reachmaine.com <linda@reachmaine.com>
2019-10-25 11:15:22 -0400

    setup blog to look reasonable

--- flatsome-defy/functions.php
+++ flatsome-defy/functions.php
@@ -63,4 +63,23 @@ if ( function_exists('register_sidebar') ){
 		return $open;
 	}
 	add_filter( 'comments_open', 'filter_media_comment_status', 10 , 2 );
+
+if ( ! function_exists( 'defy_posted_on' ) ) {
+	function defy_posted_on() {
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+    if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf( $time_string,
+        esc_attr( get_the_date( 'c' ) ),
+        esc_html( get_the_date() ),
+        esc_attr( get_the_modified_date( 'c' ) ),
+        esc_html( get_the_modified_date() )
+    );
+
+    echo '<span class="posted-on">' .  $time_string. '</span>';
+
+  }
+}
 ?>
